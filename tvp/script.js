0b50834414fbeae0e4b93165ff0a5d13cbb72183
qdnl <56455957+qdnl@users.noreply.github.com>
2025-09-17 22:23:47 +0200

    Add files via upload
    Przeniesiono formatowanie SVG do arkusza stylów, usunięto zbędne funkcje

--- tvp/script.js
+++ tvp/script.js
@@ -3,32 +3,21 @@ const
     godziny = document.getElementById('godziny'),
     minuty = document.getElementById('minuty'),
     sekundy = document.getElementById('sekundy'),
-    logo = document.getElementById('logo'),
-    /*włączanie i wyłączanie dowolnej kropki */
-    wlacz = (element) => {
-        element.classList.add("on");
-        element.classList.remove("off");
-    },
-wylacz = (element) => {
-    element.classList.add("off");
-    element.classList.remove("on");
-}
-
+    logo = document.getElementById('logo')
 
 pokazLogo = () => {
     document.addEventListener('keydown', function(zdarzenie) {
         if (zdarzenie.key === "l") {
-            if (logo.classList.contains("on"))
-                    wylacz(logo);
-                else wlacz(logo);
+            if (logo.classList.contains("on")) {
+		    logo.classList.remove("on");
+		    logo.classList.add("off");
+            }
+                else {
+			logo.classList.add("on");
+			logo.classList.remove("off");
+                }
         }
     });
-}
-
-/*wyłączanie wszystkich kropek */
-wylaczWszystko = () => {
-    for (i = 0; i < 60; i++)
-        wylacz(kropka[i])
 }
 
 obroc = (element, kat) => {
@@ -42,16 +31,6 @@ rysujAnalog = () => {
     obroc(godziny, 30 * (h % 12) + m / 2 + s / 120);
     obroc(minuty, 6 * m + s / 10);
     obroc(sekundy, 6 *s);
-
-}
-
-
-/*funkcja wysująca wskaźnik sekund wokół zegara */
-rysujSekundy = () => {
-    wylaczWszystko();
-    var czas = new Date(),
-        s = czas.getSeconds();
-    wlacz(kropka[s]);
 }
 
 zegar = () => {
